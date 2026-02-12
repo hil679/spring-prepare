e0f24f7801d7311d1f4510bdc0ddab5652c184e6
hil679 <hil679922@gmail.com>
2026-02-12 10:59:13 +0900

    Feat: JPA Auditing

--- src/main/java/com/example/springprepare/memo/dto/MemoResponseDto.java
+++ src/main/java/com/example/springprepare/memo/dto/MemoResponseDto.java
@@ -3,20 +3,28 @@ package com.example.springprepare.memo.dto;
 import com.example.springprepare.memo.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
-    }
+//    public MemoResponseDto(Long id, String username, String contents) {
+//        this.id = id;
+//        this.username = username;
+//        this.contents = contents;
+//    }
+
 }
